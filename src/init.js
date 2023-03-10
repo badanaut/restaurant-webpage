e9f592a1cf828a0c8a0e15fb429f71e26bdac9b6
badanaut <49814350+badanaut@users.noreply.github.com>
2023-03-10 17:05:06 +0200

    Add footer styles

--- src/init.js
+++ src/init.js
@@ -2,10 +2,14 @@ import {createHomeContent} from "./home";
 import {createHeader} from "./header";
 
 const pageContent = document.getElementById('content');
+const bodyPage = document.querySelector('body');
+const footer = document.createElement('footer')
+footer.innerHTML =`<p>&copy; 2023 The Odin Restaurant. All rights reserved.</p>`
 
 function initWebsite() {
   pageContent.appendChild(createHeader());
   pageContent.appendChild(createHomeContent());
+  bodyPage.appendChild(footer);
 }
 
 // eslint-disable-next-line import/prefer-default-export
