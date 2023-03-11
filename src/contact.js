1e30eca7f742a86d27393d8b9cefc3669a3a0767
badanaut <49814350+badanaut@users.noreply.github.com>
2023-03-11 20:27:27 +0200

    Add contact information

--- src/contact.js
+++ src/contact.js
@@ -3,14 +3,9 @@ function createContactContent() {
   contactContent.classList.add('contact')
   contactContent.innerHTML = `<h2>Contact Us</h2>
   <div class="contact-info">
-    <h3>Our Location</h3>
-    <p>123 Main Street<br>Anytown, USA</p>
-
-    <h3>Phone</h3>
-    <p>(123) 456-7890</p>
-
-    <h3>Email</h3>
-    <p>info@deliciousrestaurant.com</p>
+    <h3><strong>Our Location:</strong> 123 Main Street<br>Anytown, USA</h3>
+    <h3><strong>Phone:</strong> (123) 456-7890</h3>
+    <h3>Email: info@odin-restaurant.com</h3>
   </div>`;
     
   return contactContent;
